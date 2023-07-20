43516a9cd94d8708b499a38ea48489c535133f09
sudarshanpk007 <sudarshanpk007@gmail.com>
2023-07-21 02:14:17 +0530

    fix

--- components/Animation.vue
+++ components/Animation.vue
@@ -2,38 +2,42 @@
   <div>
     <div class="ellipse" id="ellipse">
       <svg
-        width="1623"
-        height="1623"
-        viewBox="0 0 1623 1623"
+        width="2000"
+        height="2000"
+        viewBox="0 0 2000 2000"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         id="svg"
       >
         <circle
+          id="c1"
           opacity="1"
           cx="811.5"
           cy="811.5"
-          r="560"
+          r="780"
           stroke="#2684FE"
           stroke-width="5"
         />
         <circle
+          id="c2"
           opacity="0.8"
           cx="811.5"
           cy="811.5"
-          r="637"
+          r="790"
           stroke="#2684FE"
           stroke-width="3"
         />
         <circle
+          id="c3"
           opacity="0.5"
           cx="811.5"
           cy="811.5"
-          r="718"
+          r="800"
           stroke="#2E6BFE"
           stroke-width="3"
         />
         <circle
+          id="c4"
           opacity="0.3"
           cx="811.5"
           cy="811.5"
@@ -43,12 +47,49 @@
         />
       </svg>
     </div>
-    <img src="/images/page1/graph.png" alt="graph" class="graph" />
+    <div class="graph-container">
+      <img src="../images/page1/graph.png" alt="graph" class="graph" />
+      <svg
+        class="graphdot"
+        width="199"
+        height="199"
+        viewBox="0 0 199 199"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          opacity="0.2"
+          cx="99.5"
+          cy="99.5"
+          r="98"
+          stroke="#248CFE"
+          stroke-width="3"
+        />
+        <circle
+          opacity="0.5"
+          cx="99.5"
+          cy="99.5"
+          r="82"
+          stroke="#2E6BFE"
+          stroke-width="3"
+        />
+        <circle
+          opacity="0.7"
+          cx="99.5"
+          cy="99.5"
+          r="67"
+          stroke="#2684FE"
+          stroke-width="3"
+        />
+        <circle cx="99.5" cy="99.5" r="50" stroke="#2684FE" stroke-width="3" />
+      </svg>
+    </div>
+
     <svg viewBox="50 100 700 700" id="pathsvg" width="100vw">
       <path
-        stroke="none"
+        stroke="red"
         fill="none"
-        d="M 400 600 L 400 50 L 665 290 L 550 450 L 600 400"
+        d="M 400 600 L 400 50 L 665 290 L 665 450 L 665 400"
       ></path>
     </svg>
   </div>
@@ -60,9 +101,9 @@ template {
 }
 #ellipse {
   position: absolute;
-  top: -90vh;
-  left: -50%;
-  height: 150vh;
+  top: -130vh;
+  left: -42.5%;
+  height: 160vh;
   width: 100%;
   display: grid;
   grid-template-columns: 1fr;
@@ -84,12 +125,21 @@ template {
     50% {transform: scale(1.1);}
     100% {transform: scale(1);}
 } */
+
 .graph {
   position: absolute;
   z-index: -1;
   top: 0;
   right: 0;
   height: 100vh;
+  opacity: 0;
+}
+.graphdot {
+  height: 22vh;
+  position: absolute;
+  right: 7.5vw;
+  top: 33vh;
+  z-index: -1;
   opacity: 0;
 }
 </style>
@@ -102,34 +152,87 @@ let props = defineProps({
 });
 const { $anime } = useNuxtApp();
 let ellipsepath = ref(null);
-let tl = ref(null);
-let t2 = ref(null);
+let ellipsetl = ref(null);
+let graphtl = ref(null);
+let graphdottl = ref(null);
+let c1tl = ref(null);
+let c2tl = ref(null);
+let c3tl = ref(null);
+let c4tl = ref(null);
 let seekValue = ref(null);
 onMounted(() => {
   ellipsepath = $anime.path("#pathsvg path");
-  tl = $anime.timeline({
+  ellipsetl = $anime.timeline({
     autoplay: false,
     easing: "easeOutExpo",
     duration: 5000,
   });
-  tl.add({
+  graphtl = $anime.timeline({
+    autoplay: false,
+    duration: 5000,
+  });
+  graphdottl = $anime.timeline({
+    autoplay: false,
+    duration: 5000,
+  });
+  c1tl = $anime.timeline({
+    autoplay: false,
+    duration: 5000,
+  });
+  c2tl = $anime.timeline({
+    autoplay: false,
+    duration: 5000,
+  });
+  c3tl = $anime.timeline({
+    autoplay: false,
+    duration: 5000,
+  });
+  c4tl = $anime.timeline({
+    autoplay: false,
+    duration: 5000,
+  });
+  ellipsetl.add({
     targets: "#ellipse",
     translateX: ellipsepath("x"),
     translateY: ellipsepath("y"),
     scale: [1, 1, 0, 0, 0],
-    opacity: [1, 1, 1, 0, 0],
+    opacity: [1, 1, -0.8, 0, 0],
+    top: ["-130vh", "0vh", "-110vh", "-90vh", "-90vh"],
     easing: "easeOutQuad",
   });
-  t2 = $anime.timeline({
-    autoplay: false,
-    duration: 5000,
-  });
-  t2.add({
+  graphtl.add({
     targets: ".graph",
     opacity: [0, 0, 1, 0, 0],
     easing: "steps(1)",
   });
-  //   tl.add({
+  graphdottl.add({
+    targets: ".graphdot",
+    opacity: [0, 0, 1, 1, 1],
+    top: ["33vh", "33vh", "33vh", "33vh", "33vh"],
+    right: ["7.5vw", "7,5vw", "7.5vw"],
+    easing: "linear",
+  });
+  c1tl.add({
+    targets: "#c1",
+    r: [780, 780, 780, 780, 780],
+    easing: "easeOutElastic",
+  });
+  c2tl.add({
+    targets: "#c2",
+    r: [790, 900, 900, 900, 900],
+    easing: "easeOutElastic",
+  });
+  c3tl.add({
+    targets: "#c3",
+    r: [800, 1000, 1000, 1000, 1000],
+    easing: "easeOutElastic",
+  });
+  c4tl.add({
+    targets: "#c4",
+    r: [810, 1100, 1100, 1100, 1100],
+    easing: "easeOutElastic",
+  });
+  //   ellipsetl.add({
   //     targets: ".graph",
   //     opacity:[0,0,1,0,0],
   //     easing: "easeOutQuad",
@@ -139,7 +242,12 @@ onMounted(() => {
 
 watch(props, (val, oldVal) => {
   console.log(props.scrollPer);
-  tl.seek(tl.duration * (props.scrollPer / 100));
-  t2.seek(t2.duration * (props.scrollPer / 100));
+  ellipsetl.seek(ellipsetl.duration * (props.scrollPer / 100));
+  graphtl.seek(graphtl.duration * (props.scrollPer / 100));
+  graphdottl.seek(graphdottl.duration * (props.scrollPer / 100));
+  c1tl.seek(c1tl.duration * (props.scrollPer / 100));
+  c2tl.seek(c2tl.duration * (props.scrollPer / 100));
+  c3tl.seek(c3tl.duration * (props.scrollPer / 100));
+  c4tl.seek(c4tl.duration * (props.scrollPer / 100));
 });
 </script>
