3d5d647f38ea59896209072c0aeb27173e350348
Kuttan69 <emmanuelpoulose@gmail.com>
2023-08-01 13:47:50 +0000

    coin

---
+++ components/Animationcoin.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="animecontainer">
+        <img id="c1" src="/images/coin/Group 3.svg" alt="">
+        <img id="c2" src="/images/coin/Group 4.svg" alt="">
+        <img id="c3" src="/images/coin/Group 5.svg" alt="">
+        <img id="c4" src="/images/coin/Group 6.svg" alt="">
+        <img id="c5" src="/images/coin/Group 7.svg" alt="">
+        <img id="c6" src="/images/coin/Group 8.svg" alt="">
+        <img id="c7" src="/images/coin/Group 9.svg" alt="">
+        <img id="c8" src="/images/coin/Group 10.svg" alt="">
+        <img id="c9" src="/images/coin/Group 13.svg" alt="">
+        <img id="c10" src="/images/coin/Group 14.svg" alt="">
+        <img id="c11" src="/images/coin/Group 14.svg" alt="">
+        <img id="c12" src="/images/coin/Group 13.svg" alt="">
+        <img id="c13" src="/images/coin/Group 10.svg" alt="">
+        <img id="c14" src="/images/coin/Group 9.svg" alt="">
+        <img id="c15" src="/images/coin/Group 8.svg" alt="">
+        <img id="c16" src="/images/coin/Group 7.svg" alt="">
+        <img id="c17" src="/images/coin/Group 6.svg" alt="">
+        <img id="c18" src="/images/coin/Group 5.svg" alt="">
+        <img id="c19" src="/images/coin/Group 4.svg" alt="">
+        <img id="c20" src="/images/coin/Group 3.svg" alt="">
+    </div>
+</template>
+<style scoped>
+
+.animecontainer{
+    height: 100vh;
+    display: grid;
+    justify-items: center;
+    align-items: center;
+    grid-template-rows: 10vh;
+}
+.animecontainer > img{
+    display: none;
+    position: relative;
+    top: 50vh;
+    grid-row-start: 1;
+    grid-column-start: 1;
+}
+/* #c1{
+    display: block;
+} */
+</style>
+<script setup lang="ts">
+
+let curr=ref(0);
+let props = defineProps({
+  scrollPer: {
+    type: Number,
+    required: true,
+  },
+});
+const { $anime } = useNuxtApp();
+let i=ref(0);
+watch(props, (val, oldVal) => {
+//   ellipsetl.seek(ellipsetl.duration * (props.scrollPer / 100));
+curr=Math.round(props.scrollPer/5);
+console.log(curr);
+for(i=0;i<20;i++)
+{
+
+    let temp=`c`+(i+1);
+    let t=document.getElementById(temp);   
+    if(i==curr)
+    {
+        t.style="display:block";
+        continue;
+    }
+    t.style="display:none";
+}
+});
+</script>
---
+++ pages/coin.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="main-background">
+    <div class="animation"><Animationcoin :scrollPer="scrollPer" /></div>
+    <!-- <div class="nav"><Navbar /></div> -->
+    <div class="container" ref="el" @scroll="handleScroll()">
+      <div id="id1"></div>
+      <div id="id2"></div>
+      <div id="id3"></div>
+      <div id="id4"></div>
+      <div id="id5"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const el = ref(null);
+let scrollPer = ref(0);
+
+const handleScroll = () => {
+  const scrollTop = el.value.scrollTop;
+  const scrollHeight = el.value.scrollHeight;
+  const clientHeight = el.value.clientHeight;
+  scrollPer.value = (scrollTop / (scrollHeight - clientHeight)) * 100;
+
+  // Increase the scroll time by 2 seconds (2000 milliseconds)
+  el.value.style.scrollBehavior = "smooth";
+  el.value.style.scrollDuration = "2000";
+//   console.log(el.value.style);
+};
+</script>
+
+<style scoped>
+.animation {
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  z-index: -1;
+}
+.container {
+  scroll-snap-type: y mandatory;
+  overflow-y: scroll;
+  height: 100vh;
+  scroll-behavior: smooth;
+}
+.container > div {
+  scroll-snap-align: start;
+  scroll-snap-stop: always !important;
+  height: 100vh;
+}
+</style>
